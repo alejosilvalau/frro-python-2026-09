d3996b18ecbb81e5a9c61a4a2b96aeed5f95d126
Alejo Silva <alejo.lautaro@hotmail.com>
2026-06-29 12:53:22 -0300

    Finalizado practico 5

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -1,19 +1,25 @@
 """Base de Datos - Creación de Clase en ORM"""
 
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
+
 
 class Socio(Base):
     """Implementar un modelo Socio a traves de Alchemy que cuente con los siguientes campos:
-        - id_socio: entero (clave primaria, auto-incremental, unico)
-        - dni: entero (unico)
-        - nombre: string (longitud 250)
-        - apellido: string (longitud 250)
+    - id_socio: entero (clave primaria, auto-incremental, unico)
+    - dni: entero (unico)
+    - nombre: string (longitud 250)
+    - apellido: string (longitud 250)
     """
-    __tablename__ = 'socios'
 
-    # Completar
-
+    __tablename__ = "socios"
+    id: Mapped[int] = mapped_column(
+        "id_socio", primary_key=True, autoincrement=True, unique=True
+    )
+    dni: Mapped[int] = mapped_column(unique=True)
+    nombre: Mapped[str] = mapped_column(String(250))
+    apellido: Mapped[str] = mapped_column(String(250))
